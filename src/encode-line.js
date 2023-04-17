c59c3738716baf24fb2d1e7aed903ee23885e5d4
Artem Baluev <active.oil5287@fastmail.com>
2023-04-17 22:45:55 +0500

    feat: complete encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(line) {
   // remove line with error and write your code here
+  let result = '';
+  let count = 0;
+  for(let i=0;i<line.length;i++){
+    if(line[i] === line[i+1]){
+      count++;
+    }else if(count === 0){
+      result += line[i];
+    }else{
+      count++;
+      result += count + line[i];
+      count = 0;
+    }
+  }
+  return result;
 }
 
 module.exports = {
